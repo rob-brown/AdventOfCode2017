72b483b08555537cf1884400e9a86183c33f26ef
Robert Brown <ammoknight@gmail.com>
2017-12-15 00:05:08 -0700

    Simplify day 15

--- lib/advent_of_code/day_15_a.ex
+++ lib/advent_of_code/day_15_a.ex
@@ -1,32 +1,29 @@
 defmodule AdventOfCode.Day15A do
 
-  def generate(input_a, input_b, rounds) do
-    stream_a = generate_stream(input_a, 16807)
-    stream_b = generate_stream(input_b, 48271)
-    Stream.zip([stream_a, stream_b])
+  def matches(input_a, input_b, rounds) do
+    [
+      generate(input_a, 16807),
+      generate(input_b, 48271),
+    ]
+    |> Stream.zip
     |> Stream.take(rounds)
-    |> Stream.map(fn {x, y} -> {<<x::64>>, <<y::64>>} end)
-    |> Stream.map(fn {x, y} -> {binary_part(x, byte_size(x), -2), binary_part(y, byte_size(y), -2)} end)
-    |> Stream.filter(fn {x, y} -> x == y end)
+    |> Stream.filter(fn {x, y} -> <<x::16>> == <<y::16>> end)
     |> Enum.count
   end
 
-  defp generate_stream(input, factor) do
+  defp generate(start, factor) do
     Stream.resource(
-      fn -> input end,
-      fn acc -> 
-        value = rem(acc * factor, 2147483647)
-        {[value], value}
-      end,
+      fn -> start end,
+      fn x -> rem(x * factor, 2147483647) |> (&{[&1], &1}).() end,
       fn _ -> :ok end)
   end
 
   def test do
-    1 = generate(65, 8921, 5)
+    1 = matches(65, 8921, 5)
     IO.puts "Test Passed"
   end
 
   def solve do
-    generate 516, 190, 40_000_000
+    matches 516, 190, 40_000_000
   end
 end
--- lib/advent_of_code/day_15_b.ex
+++ lib/advent_of_code/day_15_b.ex
@@ -1,34 +1,31 @@
 defmodule AdventOfCode.Day15B do
 
-  def generate(input_a, input_b, rounds) do
-    stream_a = generate_stream(input_a, 16807) |> Stream.filter(& rem(&1, 4) == 0)
-    stream_b = generate_stream(input_b, 48271) |> Stream.filter(& rem(&1, 8) == 0)
-    Stream.zip([stream_a, stream_b])
+  def matches(input_a, input_b, rounds) do
+    [
+      input_a |> generate(16807) |> Stream.filter(& rem(&1, 4) == 0),
+      input_b |> generate(48271) |> Stream.filter(& rem(&1, 8) == 0),
+    ]
+    |> Stream.zip
     |> Stream.take(rounds)
-    |> Stream.map(fn {x, y} -> {<<x::64>>, <<y::64>>} end)
-    |> Stream.map(fn {x, y} -> {binary_part(x, byte_size(x), -2), binary_part(y, byte_size(y), -2)} end)
-    |> Stream.filter(fn {x, y} -> x == y end)
+    |> Stream.filter(fn {x, y} -> <<x::16>> == <<y::16>> end)
     |> Enum.count
   end
 
-  defp generate_stream(input, factor) do
+  defp generate(start, factor) do
     Stream.resource(
-      fn -> input end,
-      fn acc -> 
-        value = rem(acc * factor, 2147483647)
-        {[value], value}
-      end,
+      fn -> start end,
+      fn x -> rem(x * factor, 2147483647) |> (&{[&1], &1}).() end,
       fn _ -> :ok end)
   end
 
   def test do
-    1 = generate(65, 8921, 1056)
-    309 = generate(65, 8921, 5_000_000)
+    1 = matches(65, 8921, 1056)
+    309 = matches(65, 8921, 5_000_000)
     IO.puts "Test Passed"
   end
 
   def solve do
-    generate 516, 190, 5_000_000
+    matches 516, 190, 5_000_000
   end
 end
 
