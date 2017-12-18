17c593ec1543d59890d937a6a03dc1e757954f0a
Robert Brown <ammoknight@gmail.com>
2017-12-18 09:01:17 -0700

    Simplify day 18

--- lib/advent_of_code/day_18_b.ex
+++ lib/advent_of_code/day_18_b.ex
@@ -1,6 +1,7 @@
 defmodule AdventOfCode.Day18B do
+  import Kernel, except: [apply: 3]
 
-  def run(input) do
+  def run_async(input) do
     input
     |> Stream.map(&String.trim/1)
     |> Stream.reject(& &1 == "")
@@ -14,47 +15,41 @@ defmodule AdventOfCode.Day18B do
   end
 
   defp spawn_process(commands, pid) do
-    %{state: :idle, commands: commands, pid: 0, pc: 0, env: %{"p" => pid, "queue" => [], "sent" => 0}}
+    %{state: :idle, commands: commands, pc: 0, env: %{"p" => pid, "queue" => [], "sent" => 0}}
   end
   
   defp run_till_block(processes) do
-    case next_process(processes) do
-      nil ->
+    with {pid, process} <- next_process(processes),
+      other_pid = pid == 0 && 1 || 0,
+      {state, pc, env, msg} <- step(process.commands, process.pc, process.env)
+    do
+      processes 
+      |> send_msg(msg, other_pid) 
+      |> Map.update!(pid, & %{&1 | state: state, pc: pc, env: env}) 
+      |> run_till_block
+    else
+      :none_ready ->
         processes
-      {pid, process} ->
-        {state, pc, env} = step process.commands, process.pc, process.env
-        case state do
-          {:send, value} ->
-            other_pid = pid == 0 && 1 || 0
-            other_process = Map.get(processes, other_pid)
-            new_env = other_process.env |> Map.update("queue", [], fn queue -> queue ++ [value] end)
-            run_till_block %{
-              pid => %{process | state: :idle, pc: pc, env: env}, 
-              other_pid => %{other_process | env: new_env},
-            }
-          _ ->
-            new_process = %{process | state: state, pc: pc, env: env}
-            new_processes = %{processes | pid => new_process}
-            run_till_block new_processes
-        end
     end
   end
 
   defp next_process(processes = %{}) do
-    Enum.find(processes, fn {_, process} ->
-      process.state == :idle or (process.state == :rcv and Enum.empty?(process.env["queue"]) == false)
+    Enum.find(processes, :none_ready, fn 
+      {_, %{state: :idle}} -> true
+      {_, %{state: :rcv, env: env}} -> Enum.empty?(env["queue"]) == false
+      _ -> false
     end)
   end
 
-  defp parse(["set", a, b]), do: {:set, parse(a), parse(b)}
-  defp parse(["snd", a]), do: {:snd, parse(a)}
-  defp parse(["add", a, b]), do: {:add, parse(a), parse(b)}
-  defp parse(["mul", a, b]), do: {:mul, parse(a), parse(b)}
-  defp parse(["mod", a, b]), do: {:mod, parse(a), parse(b)}
-  defp parse(["rcv", a]), do: {:rcv, parse(a)}
-  defp parse(["jgz", a, b]), do: {:jgz, parse(a), parse(b)}
+  defp parse(["set", a, b]), do: {:set, parse_arg(a), parse_arg(b)}
+  defp parse(["snd", a]),    do: {:snd, parse_arg(a)}
+  defp parse(["add", a, b]), do: {:add, parse_arg(a), parse_arg(b)}
+  defp parse(["mul", a, b]), do: {:mul, parse_arg(a), parse_arg(b)}
+  defp parse(["mod", a, b]), do: {:mod, parse_arg(a), parse_arg(b)}
+  defp parse(["rcv", a]),    do: {:rcv, parse_arg(a)}
+  defp parse(["jgz", a, b]), do: {:jgz, parse_arg(a), parse_arg(b)}
 
-  defp parse(string) do
+  defp parse_arg(string) do
     case Integer.parse(string) do
       {int, ""} ->
         int
@@ -63,59 +58,57 @@ defmodule AdventOfCode.Day18B do
     end
   end
 
-  defp step(commands, pc, env) do
-    case Enum.at(commands, pc) do
-      nil ->
-        {:term, pc, env}
-      command -> 
-        case apply_cmd(command, pc, env) do
-          {:idle, new_pc, new_env} ->
-            step commands, new_pc, new_env
-          state ->  
-            state
-        end
-    end
+  defp send_msg(processes, nil, _), do: processes
+  defp send_msg(processes, message, pid) do
+    update_in processes, [pid, :env, "queue"], (& &1 ++ [message])
   end
 
-  defp apply_cmd({:set, a, b}, pc, env) do
-    {:idle, pc + 1, Map.put(env, a, lookup(b, env))}
+  defp step(commands, pc, env) do
+    commands |> Enum.at(pc, :done) |> apply(pc, env)
   end
-  defp apply_cmd({:snd, a}, pc, env) do
+
+  defp apply(:done, pc, env) do
+    {:done, pc, env, nil}
+  end
+  defp apply({:set, a, b}, pc, env) do
+    {:idle, pc + 1, Map.put(env, a, eval(b, env)), nil}
+  end
+  defp apply({:snd, a}, pc, env) do
     new_env = env |> Map.update("sent", 0, & &1 + 1)
-    {{:send, lookup(a, env)}, pc + 1, new_env}
+    message = eval(a, env)
+    {:idle, pc + 1, new_env, message}
   end
-  defp apply_cmd({:add, a, b}, pc, env) do
-    value = lookup(a, env) + lookup(b, env)
-    {:idle, pc + 1, Map.put(env, a, value)}
+  defp apply({:add, a, b}, pc, env) do
+    value = eval(a, env) + eval(b, env)
+    {:idle, pc + 1, Map.put(env, a, value), nil}
   end
-  defp apply_cmd({:mul, a, b}, pc, env) do
-    value = lookup(a, env) * lookup(b, env)
-    {:idle, pc + 1, Map.put(env, a, value)}
+  defp apply({:mul, a, b}, pc, env) do
+    value = eval(a, env) * eval(b, env)
+    {:idle, pc + 1, Map.put(env, a, value), nil}
   end
-  defp apply_cmd({:mod, a, b}, pc, env) do
-    value = rem(lookup(a, env), lookup(b, env))
-    {:idle, pc + 1, Map.put(env, a, value)}
+  defp apply({:mod, a, b}, pc, env) do
+    value = rem(eval(a, env), eval(b, env))
+    {:idle, pc + 1, Map.put(env, a, value), nil}
   end
-  defp apply_cmd({:rcv, a}, pc, env) do
+  defp apply({:rcv, a}, pc, env) do
     case env["queue"] do
       [head | rest] ->
         new_env = env |> Map.put(a, head) |> Map.put("queue", rest)
-        {:idle, pc + 1, new_env}
+        {:idle, pc + 1, new_env, nil}
       [] ->
-        {:rcv, pc, env}
+        {:rcv, pc, env, nil}
     end
   end
-  defp apply_cmd({:jgz, a, b}, pc, env) do
-    if lookup(a, env) > 0 do
-      {:idle, pc + lookup(b, env), env}
+  defp apply({:jgz, a, b}, pc, env) do
+    if eval(a, env) > 0 do
+      {:idle, pc + eval(b, env), env, nil}
     else
-      {:idle, pc + 1, env}
+      {:idle, pc + 1, env, nil}
     end
   end
-  
 
-  defp lookup(int, _) when is_integer(int), do: int
-  defp lookup(name, env) when is_binary(name), do: Map.get(env, name, 0)
+  defp eval(int, _) when is_integer(int), do: int
+  defp eval(name, env) when is_binary(name), do: Map.get(env, name, 0)
   
   def test do
     input = """
@@ -127,7 +120,7 @@ defmodule AdventOfCode.Day18B do
     rcv c
     rcv d
     """
-    3 = input |> String.split("\n") |> run
+    3 = input |> String.split("\n") |> run_async
     IO.puts "Test Passed"
   end
 
@@ -175,7 +168,7 @@ defmodule AdventOfCode.Day18B do
     jgz f -16
     jgz a -19
     """
-    input |> String.split("\n") |> run
+    input |> String.split("\n") |> run_async
   end
 end
 
